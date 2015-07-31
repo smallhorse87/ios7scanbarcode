39b859c2840d5135809856992aadd669d67ec4b8
chenxiaosong <smallhorse87@163.com>
2015-07-31 15:50:17 +0800

    提交代码

---
+++ ios7scanbarcode/ios7scanbarcode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios7scanbarcode
+//
+//  Created by Apple on 15/7/31.
+//  Copyright (c) 2015年 Zhitian Network Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios7scanbarcode/ios7scanbarcodeTests/ios7scanbarcodeTests.m
@@ -0,0 +1,40 @@
+//
+//  ios7scanbarcodeTests.m
+//  ios7scanbarcodeTests
+//
+//  Created by Apple on 15/7/31.
+//  Copyright (c) 2015年 Zhitian Network Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ios7scanbarcodeTests : XCTestCase
+
+@end
+
+@implementation ios7scanbarcodeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
